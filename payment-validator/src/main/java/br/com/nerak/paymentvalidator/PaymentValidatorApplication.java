846d0d4fef259577259a0c2ddf032b870f84af1d
douglasjr97 <falcao.douglas97@gmail.com>
2024-06-30 09:48:34 -0300

    feat: consume message from Kafka topic

--- payment-validator/src/main/java/br/com/nerak/paymentvalidator/PaymentValidatorApplication.java
+++ payment-validator/src/main/java/br/com/nerak/paymentvalidator/PaymentValidatorApplication.java
@@ -1,9 +1,11 @@
-package br.com.nerak.payment_validator;
+package br.com.nerak.paymentvalidator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class PaymentValidatorApplication {
 
 	public static void main(String[] args) {
--- payment-validator/src/test/java/br/com/nerak/paymentvalidator/PaymentValidatorApplicationTests.java
+++ payment-validator/src/test/java/br/com/nerak/paymentvalidator/PaymentValidatorApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.nerak.payment_validator;
+package br.com.nerak.paymentvalidator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
